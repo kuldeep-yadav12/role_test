098291f23ec38536c62de64f0f9513253f73162d
anjummishra <anjummishra.mdb@gmail.com>
2025-07-14 18:08:03 +0530

    login code

--- app/Http/Controllers/Auth/LoginRegisterController.php
+++ app/Http/Controllers/Auth/LoginRegisterController.php
@@ -11,4 +11,23 @@ use Illuminate\Support\Facades\Hash;
 class LoginRegisterController extends Controller
 {
    
+
+    public function showLogin()
+    { return view('auth.login');
+     }
+
+
+   public function login(Request $request)
+   {
+       $users = $request->validate([
+           'username'  => 'required',
+           'useremail'    => 'required',
+           'userpass' => 'required',
+       ]);
+
+ return redirect()->route('register')
+         ->with('status','User is Logged In Successfully');
+
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,9 @@ Route::get('/', function () {
 });
 
 
+
+
+Route::get('/login', [LoginRegisterController::class, 'showLogin'])->name('user.login');
+Route::post('/loginUser', [LoginRegisterController::class, 'login'])->name('user');
+Route::post('/logout', [LoginRegisterController::class, 'logout'])->name('user.logout');
+
