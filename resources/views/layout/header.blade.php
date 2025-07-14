65175429c46db89c22df5accae728c5fa8abe31d
kuldeep-yadav12 <kuldeep98.mdb@gmail.com>
2025-07-14 18:10:09 +0530

    code merge

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,37 @@
+ <div id="logo">
+        <span class="big-logo">Blogs</span>
+    </div>
+    <div id="left-menu">
+        <ul>
+            <li class="active"><a href="#">
+                <i class="ion-ios-person-outline"></i>
+                <span>Dashboard</span>
+            </a></li>
+
+            <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                All Users
+            </button>
+            <ul class="dropdown-menu">
+                <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+                <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+                <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+            </ul>
+            </div>
+         <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                All Blogs
+            </button>
+            <ul class="dropdown-menu">
+                <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+                <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+                <li><button class="dropdown-item" type="button">Dropdown item</button></li>
+            </ul>
+            </div>
+
+        </ul>
+    </div>
+
+<div id="">
+       <h1>hii</h1>
+    </div>
