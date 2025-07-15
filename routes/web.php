a93a62e4f02bcfbb34f7f2ea1910a70121e0a733
kapil515 <kapilb.mdb@gmail.com>
2025-07-15 10:47:20 +0530

    upload code via kapil

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AdminController;
 use App\Http\Controllers\Auth\LoginRegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('register', [AdminController::class, 'index']);
+Route::post('/register', [AdminController::class, 'store'])->name('user.store');
 
 
