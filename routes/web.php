0734cbc927e781f43e3364bef01a944b37ea6b82
kapil515 <kapilb.mdb@gmail.com>
2025-07-15 11:35:27 +0530

    Name_update_controller

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\AdminController;
+use App\Http\Controllers\Auth\UserController;
 use App\Http\Controllers\Auth\LoginRegisterController;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('register', [AdminController::class, 'index']);
-Route::post('/register', [AdminController::class, 'store'])->name('user.store');
+Route::get('register', [UserController::class, 'index']);
+Route::post('/register', [UserController::class, 'store'])->name('user.store');
 
 
 
