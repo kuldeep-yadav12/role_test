95075b6131f4b93394e6a5df305457d5b7d6f5c3
kuldeep-yadav12 <kuldeep98.mdb@gmail.com>
2025-07-15 16:50:15 +0530

    code merge

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -31,7 +31,7 @@
             </a>
         </li>
         <li class="nav-item menu-items">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="{{ route("home") }}">
                 <span class="menu-icon">
                     <i class="mdi mdi-speedometer"></i>
                 </span>
