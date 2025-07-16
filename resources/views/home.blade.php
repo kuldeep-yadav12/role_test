a355650c26430901cc9204f0d90207c9abe906ea
kapil515 <kapilb.mdb@gmail.com>
2025-07-16 12:38:10 +0530

    merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,8 +24,6 @@
             </div>
         </div>
     @endif
-    
-    @include('partials.user-table', ['users' => $users, 'showActions' => true])
 
     <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
         <div class="card">
@@ -58,7 +56,7 @@
             <div class="alert alert-success">{{ session('success') }}</div>
         @endif
 
-        @include('partials.user-table', ['users' => $users])
+        @include('partials.user-table', ['users' => $users, 'showActions' => true])
     </div>
 @endif
 
