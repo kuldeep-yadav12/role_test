2b93694da7cd425c7100210c616942bb759e6221
kapil515 <kapilb.mdb@gmail.com>
2025-07-18 12:14:43 +0530

    jslink_update

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <title>First Project</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <div class="container-scroller">
@@ -26,6 +27,5 @@
     </div>
     @include('layout.footer')
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </html>
