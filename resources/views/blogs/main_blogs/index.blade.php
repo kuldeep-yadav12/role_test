81132d1148c34bbb4922bc1c3147a8101be7ae16
kapil515 <kapilb.mdb@gmail.com>
2025-07-21 12:05:27 +0530

    blog_tabs and filter

--- resources/views/blogs/main_blogs/index.blade.php
+++ resources/views/blogs/main_blogs/index.blade.php
@@ -1,71 +1,115 @@
 @extends('layout.app')
 
 @section('contant')
-<div class="m-5">
-    @include('partials.blog_tabs')
-    <div class="d-flex justify-content-between align-items-center mb-4">
-        <h2>All Blogs</h2>
+    <div class="m-5">
+        <div class="d-flex justify-content-between align-items-center mb-4">
+            <h2>Blogs</h2>
+        </div>
+        @include('partials.blog_filter')
+        @include('partials.blog_tabs', [
+            'blogs' => $blogs,
+            'trashedBlogs' => $trashedBlogs,
+            'activeTab' => $activeTab,
+        ])
+
     </div>
-    @include('partials.blog_filter')
 
-    <div id="blog-data">
-        @include('blogs.main_blogs.blogs')
-    </div>
-</div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
 
-<script>
-    $(document).ready(function () {
-        $(document).on('click', '.pagination a', function (e) {
-            e.preventDefault();
-
-            let url = $(this).attr('href');
-
-            $.ajax({
-                url: url,
-                type: 'GET',
-                dataType: 'html',
-                success: function (data) {
-                    $('#blog-data').html(data);
-                    window.history.pushState(null, '', url);
-                },
-                error: function () {
-                    alert('Could not load the page.');
-                }
+            // Handle Tab Switching (just updates hidden input)
+            $('.nav-link').on('click', function() {
+                let tab = $(this).attr('id') === 'trash-blogs-tab' ? 'trash' : 'all';
+                $('#active-tab-input').val(tab);
             });
-        });
 
+            // FILTER AJAX
+            $('#blog-filter-form').on('submit', function(e) {
+                e.preventDefault(); // Prevent page reload
 
-        // FILTER FORM AJAX
-        $('#blog-filter-form').on('submit', function(e) {
-            e.preventDefault();
-            let formData = $(this).serialize();
+                let formData = $(this).serialize();
+                let activeTab = $('#active-tab-input').val();
 
-            $.ajax({
-                url: "{{ route('blog.main_blog.index') }}",
-                method: 'GET',
-                data: formData,
-                success: function(response) {
-                    $('#blog-data').html(response);
-                }
+                $.ajax({
+                    url: "{{ route('blog.main_blog.index') }}",
+                    method: 'GET',
+                    data: formData,
+                    success: function(response) {
+                        if (activeTab === 'trash') {
+                            $('#trash-blogs').html(response);
+                        } else {
+                            $('#all-blogs').html(response);
+                        }
+                    },
+                    error: function() {
+                        alert('Something went wrong!');
+                    }
+                });
             });
-        });
 
-        // RESET FILTER
-        $('#reset-button').on('click', function () {
-            $('#blog-filter-form')[0].reset();
 
-            $.ajax({
-                url: "{{ route('blog.main_blog.index') }}",
-                method: 'GET',
-                success: function(response) {
-                    $('#blog-data').html(response);
-                }
+            // PAGINATION AJAX
+            $(document).on('click', '.pagination a', function(e) {
+                e.preventDefault();
+
+                let url = $(this).attr('href');
+                let tab = url.includes('trash_page') ? 'trash' : 'all';
+                $('#active-tab-input').val(tab);
+
+                $.ajax({
+                    url: url + '&tab=' + tab,
+                    type: 'GET',
+                    success: function(response) {
+                        if (tab === 'trash') {
+                            $('#trash-blogs').html(response);
+                            activateBootstrapTab('#trash-blogs-tab');
+                        } else {
+                            $('#all-blogs').html(response);
+                            activateBootstrapTab('#all-blogs-tab');
+                        }
+                        window.history.pushState(null, '', url + '&tab=' + tab);
+                    },
+                    error: function() {
+                        alert('Pagination error.');
+                    }
+                });
             });
+
+            // RESET BUTTON
+            $('#reset-button').on('click', function() {
+                $('#blog-filter-form')[0].reset();
+                let tab = $('#active-tab-input').val();
+
+                $.ajax({
+                    url: "{{ route('blog.main_blog.index') }}",
+                    method: 'GET',
+                    data: {
+                        tab: tab
+                    },
+                    success: function(response) {
+                        if (tab === 'trash') {
+                            $('#trash-blogs').html(response);
+                            activateBootstrapTab('#trash-blogs-tab');
+                        } else {
+                            $('#all-blogs').html(response);
+                            activateBootstrapTab('#all-blogs-tab');
+                        }
+                        window.history.pushState(null, '',
+                            "{{ route('blog.main_blog.index') }}?tab=" + tab);
+                    },
+                    error: function() {
+                        alert('Reset error.');
+                    }
+                });
+            });
+
+            // 🔁 Re-activate Bootstrap Tab after AJAX
+            function activateBootstrapTab(tabSelector) {
+                let tabTrigger = new bootstrap.Tab(document.querySelector(tabSelector));
+                tabTrigger.show();
+            }
         });
-    
-    });
-</script>
+    </script>
 @endsection
-
--- resources/views/partials/blog_filter.blade.php
+++ resources/views/partials/blog_filter.blade.php
@@ -1,4 +1,6 @@
-<form action="{{ route('blog.main_blog.index') }}" method="GET" class="row g-3 mb-4 align-items-center d-flex" id="blog-filter-form">
+<form id="blog-filter-form" class="row g-3 mb-4 align-items-center d-flex">
+    <input type="hidden" name="tab" id="active-tab-input" value="{{ $activeTab ?? 'all' }}">
+    
     <div class="col">
         <input type="text" name="title" class="form-control" placeholder="Name" value="{{ request('title') }}">
     </div>
@@ -8,9 +10,11 @@
     <div class="col">
         <input type="date" name="end_date" class="form-control" value="{{ request('end_date') }}">
     </div>
+
     <div class="col d-flex">
         <button type="submit" class="btn btn-primary mr-3">Filter</button>
         <button type="button" id="reset-button" class="btn btn-secondary">Reset</button>
     </div>
+
     <a href="{{ route('blog.main_blog.create') }}" class="btn btn-success">+ Add Blog</a>
 </form>
